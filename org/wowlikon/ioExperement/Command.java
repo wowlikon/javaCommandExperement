66f8e0f7a18f0d82bf348db8b4b93aff99941cb8
wowlikon <wowlikon@gmail.com>
2024-01-29 23:44:50 +0300

    added comments and small changes

--- org/wowlikon/ioExperement/Command.java
+++ org/wowlikon/ioExperement/Command.java
@@ -6,9 +6,6 @@ public interface Command {
     String getCmd();
     String getCmdHelp();
     String getHelpText();
+    default String validate(String command, String[] arguments){return null;}
     void execute(String command, String[] arguments, boolObj debug, boolObj running, List<Command> cmds);
-    default String validate(String command, String[] arguments){
-        return null;
-    }
 }
-
